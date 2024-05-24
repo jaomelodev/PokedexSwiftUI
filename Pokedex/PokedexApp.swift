696e530ff8f5d38063c08e3e3c181517a33cf730
João Mateus <joaomateusabm@yahoo.com.br>
2024-05-23 21:52:32 -0300

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by João Melo on 23/05/24.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
